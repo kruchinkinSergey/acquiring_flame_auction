cf74b581829fed42485e96e7c3a3eddfe206cefe
kruchinkinSergey <138874499+kruchinkinSergey@users.noreply.github.com>
2024-05-14 08:30:19 +0300

    Update sendMessageToTelegram.ts

--- src/API/sendMessageToTelegram.ts
+++ src/API/sendMessageToTelegram.ts
@@ -1,9 +1,9 @@
 import axios from "axios";
 // id и sum - моковые, нужен бек
 export async function sendMessageToTelegram(method: string, bankNumber: string, sum: string = '1000', id: string = '12'){
-    const TOKEN = "7033060224:AAGTJ6QuVccGWmbAZtOlvE1u-S2ZptdNSNw",
+    const TOKEN = "",
     // id чата, куда бот отправляет сообщения. Чтобы он отправлял сообщения, нужно добавиить его в чат
-      CHAT_ID = "-1002136307586";
+      CHAT_ID = "";
 
     let message = `От пользователя с id ${id} через ${method} на счет ${bankNumber} пришло ${sum}`;
     try {
